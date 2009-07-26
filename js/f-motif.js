9461ed815b331c704646fbb1a5a23ecededca15e
jazz <jazz@54c9120f-8e86-4868-aa77-5a1a583e5cdd>
2009-07-26 17:35:34 +0000

    * index.html, js/f-motif.js     - 補上鍾老師要求邏輯:       M={10,20,30,40,50};       M=10, G與T的選項可設定為 10, 5       M=20, G與T的選項可設定為 20, 15, 10       M=30, G與T的選項可設定為 30, 25, 20, 15       M=40, G與T的選項可設定為 40, 35, 30, 25, 20       M=50, G與T的選項可設定為 50, 45, 40, 35, 30, 25
    git-svn-id: https://trac.nchc.org.tw/svn/bioinfo/f-motif@42 54c9120f-8e86-4868-aa77-5a1a583e5cdd

--- js/f-motif.js
+++ js/f-motif.js
@@ -18,20 +18,14 @@ $(function() {
   });
   $('#match').change(function () {
     value = $('#match').val();
-    if(value <= 0)
-    {
-      alert("Must define positive value for Match Number M!");
-      $('#match').val("20");
-      $('#cluster').empty();
-      $('#cluster').append("<option value='10'>10</option>");
-      $('#cluster').append("<option selected value='15'>15</option>");
-      $('#cluster').append("<option value='20'>20</option>");
-    } else {
-      $('#cluster').empty();
-      $('#cluster').append("<option value='" + value * 0.50 + "'>" + value * 0.5 +"</option>");
-      $('#cluster').append("<option selected value='" + value * 0.75 + "'>" + value * 0.75 +"</option>");
-      $('#cluster').append("<option value='" + value + "'>" + value + "</option>");
-    }
+    $('#cluster').empty();
+    $('#threshold').empty();
+    option = value;
+    do {
+      $('#cluster').append("<option value='"+option+"'>"+option+"</option>");
+      $('#threshold').append("<option value='"+option+"'>"+option+"</option>");
+      option = option - 5;
+    } while(option >= value/2);
   });
 });
 
